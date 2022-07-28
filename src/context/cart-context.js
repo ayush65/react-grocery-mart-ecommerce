e0de2d1305eb23d0bf6a8a554aa4b50519e150b7
Ayush Prakash <payush381@gmail.com>
2022-07-28 10:49:32 +0530

    refracted js

---
+++ src/context/cart-context.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import { createContext, useContext, useReducer } from "react";
+import { reducerFunc, initialstate } from "../pages/Cart/CartCard/CardCart";
+
+const CartContext = createContext(null);
+
+const CartProvider = ({ children }) => {
+  const [statetotal, stateDispatch] = useReducer(reducerFunc, initialstate);
+
+  return (
+    <CartContext.Provider value={{ statetotal, stateDispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+const useCart = () => useContext(CartContext);
+export { CartProvider, useCart, CartContext };
